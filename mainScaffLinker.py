cfba448cfbdfe26cd1d38ac3520fff5dd39edbf0
Hugo-Blvr <bellavoirh@gmail.com>
2024-09-01 22:03:11 +0200

    Fonction de merge et de filtre des .paf

--- mainScaffLinker.py
+++ mainScaffLinker.py
@@ -4,31 +4,56 @@ import os
 pd.set_option('display.max_rows', None)
 
 
-def preprocess(paf_dir, NbMatch, IdSeq):
 
-    paf_files = []
-    for paf in os.listdir(paf_dir):paf_files.append(f"{paf_dir}/{paf}")
-    df=pd.DataFrame()
-    for paf_file in paf_files:    
-        df_paf = pd.read_csv(paf_file,sep='\t',header=None).iloc[:, 0:12] # récupération des informations
-        df_paf = df_paf[df_paf.iloc[:, 11] > 40].iloc[:, 0:11] # filtre sur la qualité de mapping
-        paf_name = paf_file.split('/')[-1].split('.')[0].split('_')[-1] # détermination du nom de l'échantillon
+def Merge_and_filtre(paf_dir, NbMatch, IdSeq):
+    """
+    Traite les fichiers PAF d'un répertoire donné et filtre les données selon les critères fournis.
+    
+    Args:
+        paf_dir (str): Chemin du répertoire contenant les fichiers PAF.
+        NbMatch (int): Seuil minimal pour le nombre de correspondances.
+        IdSeq (float): Seuil minimal pour le ratio d'identité de séquence.
+    
+    Returns:
+        tuple: Deux DataFrames pandas, le premier avec les données filtrées selon les critères,
+               le second avec les données restantes.
+    """
+    # Liste des fichiers PAF dans le répertoire
+    paf_files = [f"{paf_dir}/{paf}" for paf in os.listdir(paf_dir)]
+    # Liste pour stocker les DataFrames individuels
+    dataframes = []
+    
+    # Boucle sur chaque fichier PAF pour le traitement
+    for paf_file in paf_files:
+        # Chargement du fichier PAF en DataFrame
+        df_paf = pd.read_csv(paf_file, sep='\t', header=None).iloc[:, :12]
+        # Filtrage sur la qualité de mapping
+        df_paf = df_paf[df_paf.iloc[:, 11] > 40].iloc[:, :11]
+        # Extraction du nom de l'échantillon à partir du nom de fichier
+        paf_name = paf_file.split('/')[-1].split('.')[0].split('_')[-1]
+        # Ajout du nom de l'échantillon à la première colonne
         df_paf[0] = f"{paf_name}$" + df_paf[0]
-        df = pd.concat([df,df_paf])
-    
+        # Ajout du DataFrame à la liste
+        dataframes.append(df_paf)
+        
+    # Concaténation de tous les DataFrames en un seul
+    df = pd.concat(dataframes, ignore_index=True)
+    # Calcul de la nouvelle colonne 'IdSeq' (= NbMatch / NbBase)
     df['IdSeq'] = df[9] / df[10]
-    df.columns = ["Qname","Qlen","Qstart","Qstop","Strand","Tname","Tlen","Tstart","Tstop","NbMatch","NbBase","IdSeq"]
-    df = df[["Qname","Qlen","Qstart","Qstop","Strand","Tname","Tlen","Tstart","Tstop","NbMatch","NbBase","IdSeq"]]
+    # Renommage des colonnes pour plus de clarté
+    df.columns = ["Qname", "Qlen", "Qstart", "Qstop", "Strand", "Tname", "Tlen", "Tstart", "Tstop", "NbMatch", "NbBase", "IdSeq"]
     
+    # Filtrage des DataFrames selon les critères NbMatch et IdSeq
     df_filtre = df[(df["NbMatch"] >= NbMatch) & (df["IdSeq"] >= IdSeq)]
-    df = df[(df["NbMatch"] < NbMatch) | (df["IdSeq"] < IdSeq)]
+    df_reste = df[(df["NbMatch"] < NbMatch) | (df["IdSeq"] < IdSeq)]
     
-    return df.reset_index(drop=True), df_filtre.reset_index(drop=True)
+    # Retourner les DataFrames filtrés et non filtrés
+    return df_reste.reset_index(drop=True), df_filtre.reset_index(drop=True)
 
 
 
 def Run(paf_dir, NbMatch, IdSeq):  
-    df, df_filtre=preprocess(paf_dir, NbMatch, IdSeq)
+    df, df_filtre=Merge_and_filtre(paf_dir, NbMatch, IdSeq)
 
     print(df)
 
